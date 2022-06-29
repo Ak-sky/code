e9a4e8ef595b563ed769a6d7dfb59e8cdac32e5b
Akash <akashkumar03@gmail.com>
2022-06-29 16:00:13 +0530

    added basic auth

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.9-bullseye
+
+# Working Directory
+WORKDIR /app
+
+# Copy source code to working directory
+COPY . /app/
+
+#Install packages
+RUN pip install -r requirements.txt
+
+#Export port
+EXPOSE 80
+
+# Run app.py at container launch
+ENTRYPOINT ["gunicorn", "-b", ":8080", "main:app"]
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "akash": generate_password_hash("ibm")
+}
 
 countries = [
     {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
@@ -8,16 +16,24 @@ countries = [
     {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
 ]
 
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+
 def create_next_id(): 
     return max(country["id"] for country in countries) + 1
 
 
 @app.get("/countries")
+@auth.login_required
 def get_countries():
     return jsonify(countries)
 
 
 @app.post("/countries")
+@auth.login_required
 def add_country():
     if request.is_json:
         country = request.get_json()
@@ -25,3 +41,7 @@ def add_country():
         countries.append(country)
         return country, 201
     return {"error": "Request must be JSON"}, 415
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
